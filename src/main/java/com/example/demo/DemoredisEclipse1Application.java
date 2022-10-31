600bf1ceda0f65649cd11ea4c890894a4f589f77
Minhhong1612 <minhhong1612@gmail.com>
2022-10-31 10:39:25 +0700

    demo redis spring boot

---
+++ src/main/java/com/example/demo/DemoredisEclipse1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoredisEclipse1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoredisEclipse1Application.class, args);
+	}
+
+}
